9da763caf651adaf666ae99020510c02f2d64c12
Howard Moseley <howard@moseleyll.com>
2015-12-19 22:58:19 -0500

    12-24

--- src/main.lua
+++ src/main.lua
@@ -9,19 +9,6 @@ local json = require 'json'
  
 channel.answer()
  
-local zip = channel.gather({play=speech("Enter your 5-digit zip code to hear current weather conditions"), maxDigits=5, minDigits=5})
- 
-local params = {q=zip, units="imperial", mode="json"}
- 
-local res, err = http.get('http://api.openweathermap.org/data/2.5/weather', {data=params})
- 
-if err then
-    channel.say('Unable to contact weather service, goodbye.')
-    channel.hangup()
-end
- 
-local res_content = json:decode(res.content)
- 
 local my_ivr = menu()
  
 function the_temperature()
